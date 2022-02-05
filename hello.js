ab121c89ca0c62289f22a64cd933e169cb88b05d
adanchet-su <alexinaancheta@gmail.com>
2022-02-05 15:11:38 +0800

    Unit_Testing_Activity

--- hello.js
+++ hello.js
@@ -1,3 +1,21 @@
 function hello() {
   return "Hello World!";
 }
+
+function greet(name) {
+  var greeting = "Hello, ";
+if (name == null) {
+  return greeting + "there!"
+}
+else {
+  if (name === name.toUpperCase()) {
+      return greeting.toUpperCase() + name + "!";
+  }
+  if (typeof name === typeof[]) {
+      for (var i = 0; i <= name.length; i +=1) {
+          greeting += name[i] + ", ";
+      }
+  }
+  return greeting + name;
+}
+}
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,18 @@
 var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+//var hello = utils.load('./hello.js', 'hello');
 
-test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+// test('outputs the correct string', () => {
+//   expect(hello()).toBe("Hello World!");
+// });
+
+//var utils = require('course-utilities');
+var greeting = utils.load('./hello.js', 'greet');
+//test1
+describe('test greet()', function () {
+    it('should be called', function () {
+        expect(greet('Lexine')).toBeCalled();
+    });
+    it('passing in single name', function () {
+        expect(greet('Lexine')).toEqual("Hello, 1Lexine");
+    })
 });
